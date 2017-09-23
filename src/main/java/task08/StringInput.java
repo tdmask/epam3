0d0f04427ce762c09b8b528b508d35c414b2973c
lenovo8c <tdmask@gmail.com>
2017-09-23 19:21:40 +0300

    task08. correction

--- src/main/java/task08/StringInput.java
+++ src/main/java/task08/StringInput.java
@@ -6,15 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class StringInput extends HttpServlet {
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response)
-            throws ServletException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         performTask(request, response);
     }
 
-    private void performTask(HttpServletRequest request,
-                             HttpServletResponse response) {
+    private void performTask(HttpServletRequest request, HttpServletResponse response) {
         StringOutput.generate(request, response);
     }
 }
